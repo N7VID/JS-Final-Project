6314305719c82b05044e55dcca50818acfd5a8be
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-06-30 14:45:08 +0330

    fix: change function structure

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,23 +1,22 @@
-var variantClasses = {
-  type1:
-    "bg-[#212529] w-[380px] h-[47px] rounded-[30px] font-medium text-[14px]",
-};
-
-export const Button = function ({
-  content,
-  variant,
-  additional,
-  ...restProps
-}) {
-  const className = variantClasses[variant];
+export function Button({ content, variant, additional, ...restProps }) {
+  const variantClasses = {
+    type1:
+      "bg-[#212529] w-[380px] h-[47px] rounded-[30px] font-medium text-[14px]",
+  };
+  const attributesString = (props) => {
+    return Object.entries(props)
+      .map(([key, value]) => `${key}="${value}"`)
+      .join(" ");
+  };
+  const className = variantClasses[variant] || "";
   const div = document.createElement("div");
   div.innerHTML = `
       <button 
       class="text-white ${className} ${additional}"
-      ${{ ...restProps }}
+      ${attributesString(restProps)}
       >
       ${content}
       </button>
     `;
   return div;
-};
+}
