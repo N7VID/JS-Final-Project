0fd02d20c4fc9aa20e6ba208567fd913193adbe8
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-06-30 20:01:54 +0330

    fix: font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,12 @@ export default {
     screens: {
       mobile: "320px",
       tablet: "640px",
-      // => @media (min-width: 640px) { ... }
-
       laptop: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
       desktop: "1280px",
-      // => @media (min-width: 1280px) { ... }
     },
     extend: {
       fontFamily: {
-        inter: ["Inter", "sans-serif"],
+        inter: ["inter", "sans-serif"],
       },
       backgroundImage: {
         "welcome-image": "url('/images/WallpaperDog-20534610 1.png')",
