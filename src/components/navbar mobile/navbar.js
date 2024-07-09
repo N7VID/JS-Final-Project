52bf2bb786c2c799dac7b84e96b5f3e6e867b5bd
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-07-09 09:31:55 +0330

    fix: bug navbar style

--- src/components/navbar mobile/navbar.js
+++ src/components/navbar mobile/navbar.js
@@ -1,62 +1,62 @@
-const icons = {
-  home: "/public/images/home-outline.svg",
-  cart: "/public/images/cart-outline.svg",
-  orders: "/public/images/shop-outline.svg",
-  wallet: "/public/images/wallet-outline.svg",
-  profile: "/public/images/profile-outline.svg",
-};
-const outlineIcons = {
-  home: "/public/images/home-outline.svg",
-  cart: "/public/images/cart-outline.svg",
-  orders: "/public/images/shop-outline.svg",
-  wallet: "/public/images/wallet-outline.svg",
-  profile: "/public/images/profile-outline.svg",
-};
-
 export function NavBar() {
-  handleActiveIcon();
+  const iconsSrc = {
+    home: "/public/images/home-outline.svg",
+    cart: "/public/images/cart-outline.svg",
+    orders: "/public/images/shop-outline.svg",
+    wallet: "/public/images/wallet-outline.svg",
+    profile: "/public/images/profile-outline.svg",
+  };
+
   const div = document.createElement("div");
   div.classList = "w-full fixed bg-white bottom-0";
   div.innerHTML = `
     <nav class="my-0 mx-auto w-full">
         <ul class="flex gap-12 justify-center items-center px-6 py-4 text-[#152536]">
-          <a href="/" data-navigo class="text-center flex flex-col cursor-pointer" id="home">
-            <img src=${icons.home} class="w-[30px]">
+          <a id="/" href="/" data-navigo class="icon home text-center flex flex-col cursor-pointer">
+            <img src=${iconsSrc.home} class="w-[30px]">
             <span class="text-[10px] font-semibold tracking-tight">Home</span>
           </a>
-          <a href="/cart" data-navigo class="text-center flex flex-col cursor-pointer" id="cart">
-            <img src=${icons.cart} class="w-[30px]">
+          <a id="/cart" href="/cart" data-navigo class="icon cart text-center flex flex-col cursor-pointer">
+            <img src=${iconsSrc.cart} class="w-[30px]">
             <span class="text-[10px] font-semibold tracking-tight">Cart</span>
           </a>
-          <li class="text-center flex flex-col cursor-pointer" id="orders">
-            <img src=${icons.orders} class="w-[30px]">
+          <a id="/orders" class="icon orders text-center flex flex-col cursor-pointer">
+            <img src=${iconsSrc.orders} class="w-[30px]">
             <span class="text-[10px] font-semibold tracking-tight">Orders</span>
-          </li>
-          <li class="text-center flex flex-col cursor-pointer" id="wallet">
-            <img src=${icons.wallet} class="w-[30px]">
+          </a>
+          <a id="/wallet" class="icon wallet text-center flex flex-col cursor-pointer">
+            <img src=${iconsSrc.wallet} class="w-[30px]">
             <span class="text-[10px] font-semibold tracking-tight">Wallet</span>
-          </li>
-          <li class="text-center flex flex-col cursor-pointer" id="profile">
-            <img src=${icons.profile} class="w-[30px]">
+          </a>
+          <a id="/profile" class="icon profile text-center flex flex-col cursor-pointer">
+            <img src=${iconsSrc.profile} class="w-[30px]">
             <span class="text-[10px] font-semibold tracking-tight">Profile</span>
-          </li>
+          </a>
         </ul>
     </nav>
   `;
+
+  const icons = Object.values(div.querySelectorAll(".icon"));
+  icons.forEach((icon) => {
+    let iconName = icon.querySelector("span").innerHTML.toLowerCase();
+    if (window.location.pathname == icon.id) {
+      icon.querySelector("img").src = `/public/images/${iconName}-solid.svg`;
+    }
+  });
 
   return div;
 }
 
 export function handleNavbarStyle() {
-  const homeBtn = document.querySelector("#home");
+  const homeBtn = document.querySelector(".home");
   const homeIcon = homeBtn.querySelector("img");
-  const cartBtn = document.querySelector("#cart");
+  const cartBtn = document.querySelector(".cart");
   const cartIcon = cartBtn.querySelector("img");
-  const ordersBtn = document.querySelector("#orders");
+  const ordersBtn = document.querySelector(".orders");
   const orderIcon = ordersBtn.querySelector("img");
-  const walletBtn = document.querySelector("#wallet");
+  const walletBtn = document.querySelector(".wallet");
   const walletIcon = walletBtn.querySelector("img");
-  const profileBtn = document.querySelector("#profile");
+  const profileBtn = document.querySelector(".profile");
   const profileIcon = profileBtn.querySelector("img");
 
   homeBtn.addEventListener("click", () => {
@@ -68,7 +68,7 @@ export function handleNavbarStyle() {
     clearIcon("cartBtn");
   });
   ordersBtn.addEventListener("click", () => {
-    ordersBtn.querySelector("img").src = "/public/images/shop-solid.svg";
+    ordersBtn.querySelector("img").src = "/public/images/orders-solid.svg";
     clearIcon("ordersBtn");
   });
   walletBtn.addEventListener("click", () => {
@@ -81,6 +81,13 @@ export function handleNavbarStyle() {
   });
 
   function clearIcon(text) {
+    const outlineIcons = {
+      home: "/public/images/home-outline.svg",
+      cart: "/public/images/cart-outline.svg",
+      orders: "/public/images/shop-outline.svg",
+      wallet: "/public/images/wallet-outline.svg",
+      profile: "/public/images/profile-outline.svg",
+    };
     switch (text) {
       case "homeBtn":
         cartIcon.src = outlineIcons.cart;
@@ -116,18 +123,4 @@ export function handleNavbarStyle() {
         break;
     }
   }
-}
-
-function handleActiveIcon() {
-  window.location.pathname === "/"
-    ? (icons.home = "/public/images/home-solid.svg")
-    : window.location.pathname === "/cart"
-    ? (icons.cart = "/public/images/cart-solid.svg")
-    : window.location.pathname === "/orders"
-    ? (icons.orders = "/public/images/shop-solid.svg")
-    : window.location.pathname === "/wallet"
-    ? (icons.wallet = "/public/images/wallet-solid.svg")
-    : window.location.pathname === "/profile"
-    ? (icons.profile = "/public/images/profile-solid.svg")
-    : icons;
 }
